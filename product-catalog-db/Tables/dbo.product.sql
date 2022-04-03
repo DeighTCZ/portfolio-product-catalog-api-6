32f61dc6ce635f2cd39f95341b521c6ab42cb07e
DeighT <j.posmura@gmail.com>
2022-04-03 14:35:44 +0200

    - Přidání readme. - Vytvoření SQL struktury podle zadání - Přidání init dat

---
+++ product-catalog-db/Tables/dbo.product.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[product]
+(
+	[id] BIGINT IDENTITY(1,1) NOT NULL, 
+    [product_name] NVARCHAR(64) NOT NULL, 
+    [description] NVARCHAR(1000) NOT NULL, 
+    [image_uri] NVARCHAR(50) NULL, 
+    [price] DECIMAL(18, 2) NOT NULL
+)
+GO
+ALTER TABLE dbo.product ADD CONSTRAINT pk_dbo_product PRIMARY KEY CLUSTERED (id)
+GO
+ALTER TABLE dbo.product ADD CONSTRAINT uk_dbo_product UNIQUE NONCLUSTERED (product_name)
+GO
---
+++ product-catalog-db/Tables/security.user.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [security].[user]
+(
+	[id] BIGINT IDENTITY(1,1) NOT NULL,
+    [login] NVARCHAR(64) NOT NULL, 
+    [password] NVARCHAR(64) NOT NULL, 
+)
+GO
+ALTER TABLE [security].[user] ADD CONSTRAINT pk_security_user PRIMARY KEY CLUSTERED (id)
+GO
+ALTER TABLE [security].[user] ADD CONSTRAINT uk_security_user UNIQUE NONCLUSTERED ([login])
+GO
