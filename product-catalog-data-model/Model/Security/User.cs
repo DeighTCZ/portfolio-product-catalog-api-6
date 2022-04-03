5565e0be02a9deb7a031e59517402390eddc2738
DeighT <j.posmura@gmail.com>
2022-04-03 15:03:29 +0200

    První implementace modelu.

---
+++ product-catalog-data-model/Model/Security/User.cs
@@ -0,0 +1,22 @@
+﻿namespace product_catalog_data_model.Model.Security;
+
+/// <summary>
+/// Model pro uživatele pro potřeby aplikace
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Id
+    /// </summary>
+    public long Id { get; init; }
+
+    /// <summary>
+    /// Login uživatele
+    /// </summary>
+    public string Login { get; init; }
+
+    /// <summary>
+    /// Heslo uživatele
+    /// </summary>
+    public string Password { get; init; }
+}
