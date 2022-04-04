837db8b7c4d14c7e28d4e4ff4c7d54fea600506f
DeighT <j.posmura@gmail.com>
2022-04-04 19:43:40 +0200

    První implementace jednoduchého controlleru.

---
+++ product-catalog-api/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.Mime;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using product_catalog_data_access.Interfaces;
+using product_catalog_data_model.Dto;
+
+namespace product_catalog_api.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductDao _dao;
+
+    private readonly IMapper _mapper;
+
+    public ProductController(IMapper mapper, IProductDao dao)
+    {
+        _mapper = mapper;
+        _dao = dao;
+    }
+
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public IActionResult GetProducts()
+    {
+        var items = _dao.GetAll();
+
+        var result = _mapper.Map<IEnumerable<product_catalog_data_model.Model.Product>, IEnumerable<Product>>(items);
+
+        return Ok(result);
+    }
+
+    [HttpGet("{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public IActionResult GetProduct(long id)
+    {
+        var item = _dao.GetById(id);
+
+        var result = _mapper.Map<product_catalog_data_model.Model.Product>(item);
+
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public IActionResult Create([FromBody] Product product)
+    {
+        var item = _mapper.Map<product_catalog_data_model.Model.Product>(product);
+
+        _dao.Create(item);
+
+        return Ok();
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public IActionResult Update([FromBody] Product product)
+    {
+        var item = _mapper.Map<product_catalog_data_model.Model.Product>(product);
+
+        _dao.Update(item);
+
+        return Ok();
+    }
+
+    [HttpDelete("{id:long}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Consumes(MediaTypeNames.Application.Json)]
+    public IActionResult Delete(long id)
+    {
+        _dao.Delete(id);
+
+        return Ok();
+    }
+}
---
+++ product-catalog-api/Startup.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using product_catalog_data_access.Dao.EF;
+using product_catalog_data_access.Dao.Mock;
+using product_catalog_data_access.EfModels;
+using product_catalog_data_access.EfModels.Automapper;
+using product_catalog_data_access.Interfaces;
+using product_catalog_data_model.Automapper;
+
+namespace product_catalog_api;
+
+public static class Startup
+{
+    public static void AddEfImplementation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ProductCatalogDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("EfProductCatalogContext")));
+
+        builder.Services.AddTransient<IProductDao, EfProductDao>();
+    }
+
+    public static void AddMockImplementation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<IProductDao, MockProductDao>();
+    }
+
+    public static void CreateAutomapperConfig(this WebApplicationBuilder builder)
+    {
+        var mapperConfig = new MapperConfiguration(mc =>
+        {
+            mc.AddProfile(new ProductMapperProfile());
+            mc.AddProfile(new UserMapperProfile());
+            mc.AddProfile(new DtoMapperProfile());
+        });
+
+        mapperConfig.AssertConfigurationIsValid();
+
+        var mapper = mapperConfig.CreateMapper();
+
+        builder.Services.AddSingleton(mapper);
+    }
+}
