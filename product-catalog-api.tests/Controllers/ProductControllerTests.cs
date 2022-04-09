a5cbdfc5ed8835409f26a1f3f497981603be5584
DeighT <j.posmura@gmail.com>
2022-04-09 13:56:35 +0200

    Základní testy.

---
+++ product-catalog-api.tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using product_catalog_api.Controllers;
+using product_catalog_api.Services;
+using product_catalog_api.tests.MockData;
+using Xunit;
+
+namespace product_catalog_api.tests.Controllers;
+
+public class ProductControllerTests
+{
+    [Fact]
+    public async Task GetAll_ShouldReturn200Status()
+    {
+        // Arrange
+        var productService = new Mock<IProductService>();
+        var mapper = new Mock<IMapper>();
+        var mockDataProvider = new ProductsMockData();
+        productService.Setup(_ => _.GetAllProducts()).Returns(mockDataProvider.GetAll());
+        var sut = new ProductController(mapper.Object, productService.Object);
+
+        // Act
+        var result = (OkObjectResult)sut.GetProducts();
+
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+}
---
+++ product-catalog-api.tests/MockData/ProductsMockData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using product_catalog_data_model.Model;
+using product_catalog_mock_data;
+
+namespace product_catalog_api.tests.MockData;
+
+public class ProductsMockData
+{
+    private readonly ProductMockDataProvider _productsProvider;
+
+    public ProductsMockData()
+    {
+        _productsProvider = new ProductMockDataProvider();
+    }
+
+    public IEnumerable<Product> GetAll()
+    {
+        return _productsProvider.GetAll();
+    }
+}
